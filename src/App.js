d6ebfb9c68eb11d87428fb8b8d4b1240881aeee6
Guillermo <guilleandmont@gmail.com>
2021-03-24 17:12:10 -0400

    got display working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="App container">
+            <div id="display" class="display">
+                Some numbers
+            </div>
+            <div id="clear" class="operation">
+                C
+            </div>
+            <div class="operation">E</div>
+            <div class="operation">/</div>
+            <div class="operation">*</div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+            <div class="operation">-</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div class="operation">+</div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div id="equals">=</div>
+            <div id="zero">0</div>
+            <div>.</div>
+        </main>
+    );
 }
 
 export default App;
