b82fc4f6b535a3fd4dbdc68420d356f1924cbd1b
Guillermo <guilleandmont@gmail.com>
2021-03-25 15:34:55 -0400

    decided to restart

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
+import React from "react";
 import "./App.css";
+import {
+    FaBackspace,
+    FaDivide,
+    FaEquals,
+    FaMinus,
+    FaPlus,
+    FaTimes,
+} from "react-icons/fa";
 
-function App() {
-    return (
-        <main className="App container">
-            <div id="display" class="display">
-                Some numbers
-            </div>
-            <div id="clear" class="operation">
-                C
-            </div>
-            <div class="operation">E</div>
-            <div class="operation">/</div>
-            <div class="operation">*</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div class="operation">-</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div class="operation">+</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div id="equals">=</div>
-            <div id="zero">0</div>
-            <div>.</div>
-        </main>
-    );
+class App extends React.Component {
+    render() {
+        return <div>Nothing at all</div>;
+    }
 }
 
 export default App;
