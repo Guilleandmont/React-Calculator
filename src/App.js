a2530eb704615ac814ecf44bff8298409f1a314f
Guillermo <guilleandmont@gmail.com>
2021-03-27 19:02:16 -0400

    started adding functions

--- src/App.js
+++ src/App.js
@@ -9,54 +9,153 @@ import {
     FaTimes,
 } from "react-icons/fa";
 
-const Numbers = ({ symbol, id }) => {
+const Numbers = ({ symbol, id, onClick }) => {
     return (
-        <div className="button" id={id}>
+        <div className="button" id={id} onClick={() => onClick(symbol)}>
             {symbol}
         </div>
     );
 };
 
-const Operations = ({ symbol, id }) => {
+const Operations = ({ symbol, id, onClick, operation }) => {
     return (
-        <div className="button operation" id={id}>
+        <div
+            className="button operation"
+            id={id}
+            onClick={() => onClick(operation)}
+        >
             {symbol}
         </div>
     );
 };
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            operation: "",
+            result: "",
+        };
+        this.handleNumbers = this.handleNumbers.bind(this);
+        this.handleClear = this.handleClear.bind(this);
+        this.handleBackspace = this.handleBackspace.bind(this);
+        this.handleOperations = this.handleOperations.bind(this);
+    }
+
+    handleNumbers(number) {
+        this.setState((state) => ({
+            result: state.result + number,
+        }));
+    }
+
+    handleClear() {
+        this.setState(() => ({
+            result: "",
+            operation: "",
+        }));
+    }
+
+    handleBackspace() {
+        this.setState((state) => ({
+            result: state.result.slice(0, state.result.length - 1),
+        }));
+    }
+
+    handleOperations(operation) {
+        this.setState((state) => ({
+            result: state.result + operation,
+        }));
+    }
+
     render() {
         return (
             <main>
                 <div className="display-container">
-                    <div className="operation">3 + 5 * 6 - 2 / 4</div>
-                    <div className="result">{3 + 5 * 6 - 2 / 4}</div>
+                    <div className="operation">{this.state.operation}</div>
+                    <div className="result">
+                        {this.state.result === "" ? 0 : this.state.result}
+                    </div>
                 </div>
                 <div className="grid-container">
-                    <Operations symbol="C" id="clear" />
-                    <Operations symbol={<FaBackspace />} id="backspace" />
-                    <Operations symbol={<FaDivide />} id="divide" />
-                    <Operations symbol={<FaTimes />} id="multiply" />
+                    <Operations
+                        symbol="C"
+                        id="clear"
+                        onClick={this.handleClear}
+                    />
+                    <Operations
+                        symbol={<FaBackspace />}
+                        id="backspace"
+                        onClick={this.handleBackspace}
+                    />
+                    <Operations
+                        symbol={<FaDivide />}
+                        id="divide"
+                        operation=" / "
+                        onClick={this.handleOperations}
+                    />
+                    <Operations
+                        symbol={<FaTimes />}
+                        id="multiply"
+                        operation=" * "
+                        onClick={this.handleOperations}
+                    />
 
-                    <Numbers symbol="7" id="seven" />
-                    <Numbers symbol="8" id="eight" />
-                    <Numbers symbol="9" id="nine" />
+                    <Numbers
+                        symbol="7"
+                        id="seven"
+                        onClick={this.handleNumbers}
+                    />
+                    <Numbers
+                        symbol="8"
+                        id="eight"
+                        onClick={this.handleNumbers}
+                    />
+                    <Numbers
+                        symbol="9"
+                        id="nine"
+                        onClick={this.handleNumbers}
+                    />
 
-                    <Operations symbol={<FaPlus />} id="substract" />
+                    <Operations
+                        symbol={<FaMinus />}
+                        id="substract"
+                        operation=" - "
+                        onClick={this.handleOperations}
+                    />
 
-                    <Numbers symbol="4" id="four" />
-                    <Numbers symbol="5" id="five" />
-                    <Numbers symbol="6" id="six" />
+                    <Numbers
+                        symbol="4"
+                        id="four"
+                        onClick={this.handleNumbers}
+                    />
+                    <Numbers
+                        symbol="5"
+                        id="five"
+                        onClick={this.handleNumbers}
+                    />
+                    <Numbers symbol="6" id="six" onClick={this.handleNumbers} />
 
-                    <Operations symbol={<FaMinus />} id="add" />
+                    <Operations
+                        symbol={<FaPlus />}
+                        id="add"
+                        operation=" + "
+                        onClick={this.handleOperations}
+                    />
 
-                    <Numbers symbol="1" id="two" />
-                    <Numbers symbol="2" id="one" />
-                    <Numbers symbol="3" id="three" />
+                    <Numbers symbol="1" id="two" onClick={this.handleNumbers} />
+                    <Numbers symbol="2" id="one" onClick={this.handleNumbers} />
+                    <Numbers
+                        symbol="3"
+                        id="three"
+                        onClick={this.handleNumbers}
+                    />
 
                     <Operations symbol={<FaEquals />} id="equals" />
-                    <Numbers symbol="0" id="zero" />
+                    <Numbers
+                        symbol="0"
+                        id="zero"
+                        onClick={this.handleNumbers}
+                    />
                     <Numbers symbol="." id="decimal" />
                 </div>
             </main>
